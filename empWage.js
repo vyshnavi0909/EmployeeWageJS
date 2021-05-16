7f8961ce8a6c55ae3ee06d085ceb6555d6d002bc
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-05-16 22:45:44 +0530

    ADD: preformed array operations

--- empWage.js
+++ empWage.js
@@ -24,6 +24,43 @@ function calDailyWage(empHrs){
     return empHrs * WAGE_PER_HR;
 }
 
+function sum(dailyWage){
+    totalEmpWage += dailyWage;
+}
+
+function totalWages(totalWage, dailyWage){
+    return totalWage + dailyWage;
+}
+
+function totalDaysWorked(numOfDays, dailyWage) {
+    if (dailyWage > 0) {
+        return numOfDays + 1;
+    }
+    return numOfDays;
+}
+
+function mapDayWithWage(dailyWage){
+    dailyCntr++;
+    return dailyCntr + " = " + dailyWage;
+}
+
+function findFullTimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+
+function fullTimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+
+function isAllFullTimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+
+function isAnyPartTimeWage(dailyWage){
+    return dailyWage.includes("80");
+}
+
+let totalEmpWage = 0;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empWageArray = new Array();
@@ -36,5 +73,20 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
     empWageArray.push(calDailyWage(empHrs));
 }
 
-let empWage = calDailyWage(totalEmpHrs);
-console.log("Employee's total working days:" + totalWorkingDays + ", total hours: " + totalEmpHrs + " and total wage: " + empWage);
+empWageArray.forEach(sum);
+
+let dailyCntr = 0;
+let mapDayWithWageArray = empWageArray.map(mapDayWithWage);
+let fullDayWageArray = mapDayWithWageArray.filter(fullTimeWage);
+
+
+console.log("Employee's total working days:" + totalWorkingDays + ", total hours: " + totalEmpHrs + " and total wage: " + totalEmpWage);
+console.log("Employee wage with reduce: " + empWageArray.reduce(totalWages, 0));
+console.log("Daily Wage Map:");
+console.log(mapDayWithWageArray);
+console.log("Daily Wage filter when full time wage earned:");
+console.log(fullDayWageArray);
+console.log("First time full time wage was earned on day: " + mapDayWithWageArray.find(findFullTimeWage));
+console.log("Check all element have full time wage: " + fullDayWageArray.every(isAllFullTimeWage));
+console.log("Check if any part time wage: " + mapDayWithWageArray.some(isAnyPartTimeWage));
+console.log("Number of days employee worked: "+ empWageArray.reduce(totalDaysWorked, 0));
