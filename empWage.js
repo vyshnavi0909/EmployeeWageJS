8d55de275c12c73fe03079b74cb315f7d53e21a6
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-05-16 22:09:37 +0530

    ADD: calculated wage for 20 days

--- empWage.js
+++ empWage.js
@@ -1,3 +1,9 @@
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+const NUM_OF_WORKING_DAYS = 20; 
 
 function getWorkingHrs(empCheck){
     switch(empCheck){
@@ -13,16 +19,10 @@ function getWorkingHrs(empCheck){
     return empHrs;
 }
 
-
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HRS = 4;
-const FULL_TIME_HRS = 8;
-const WAGE_PER_HR = 20;
-
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10 ) % 3;
-empHrs = getWorkingHrs(empCheck);
-
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() * 10 ) % 3;
+    empHrs += getWorkingHrs(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HR;
-console.log("Employee Wage: " + empWage);
+console.log("Employee total hours: " + empHrs + " and total wage: " + empWage);
