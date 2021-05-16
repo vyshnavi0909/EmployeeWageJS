686451816fa18856480021b7e0924bfb52935bba
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-05-16 22:18:25 +0530

    ADD: calculating wage till total days or total hours reached

--- empWage.js
+++ empWage.js
@@ -3,7 +3,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
-const NUM_OF_WORKING_DAYS = 20; 
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
 
 function getWorkingHrs(empCheck){
     switch(empCheck){
@@ -19,10 +20,12 @@ function getWorkingHrs(empCheck){
     return empHrs;
 }
 
-let empHrs = 0;
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10 ) % 3;
-    empHrs += getWorkingHrs(empCheck);
+    totalEmpHrs += getWorkingHrs(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HR;
-console.log("Employee total hours: " + empHrs + " and total wage: " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HR;
+console.log("Employee's total working days:" + totalWorkingDays + ", total hours: " + totalEmpHrs + " and total wage: " + empWage);
