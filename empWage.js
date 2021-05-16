5cede636414c7b88769e16b0e738a5189e81ec63
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-05-16 21:46:50 +0530

    ADD: checking employee attendence

---
+++ empWage.js
@@ -0,0 +1,8 @@
+const IS_ABSENT = 0;
+
+let empCheck = Math.floor( Math.random() * 10 ) % 2;
+if(empCheck == IS_ABSENT){
+    console.log("Employee is absent");
+}else{
+    console.log("Employee is present");
+}
