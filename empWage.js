ce337d04c2868905ddccbdea3a6250079384cad7
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-05-16 21:59:56 +0530

    ADD: calculating daily wage

--- empWage.js
+++ empWage.js
@@ -1,8 +1,22 @@
-const IS_ABSENT = 0;
 
-let empCheck = Math.floor( Math.random() * 10 ) % 2;
-if(empCheck == IS_ABSENT){
-    console.log("Employee is absent");
-}else{
-    console.log("Employee is present");
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10 ) % 3;
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HRS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HRS;
+        break;
+    default:
+        empHrs = 0;
 }
+
+let empWage = empHrs * WAGE_PER_HR;
+console.log("Employee Wage: " + empWage);
