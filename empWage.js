29fda0ffcce17840f1969a1ebfe4a0c8b82e40c2
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-05-16 22:05:03 +0530

    ADD: function to get working hours

--- empWage.js
+++ empWage.js
@@ -1,4 +1,19 @@
 
+function getWorkingHrs(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            empHrs = PART_TIME_HRS;
+            break;
+        case IS_FULL_TIME:
+            empHrs = FULL_TIME_HRS;
+            break;
+        default:
+            empHrs = 0;
+    }
+    return empHrs;
+}
+
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
@@ -7,16 +22,7 @@ const WAGE_PER_HR = 20;
 
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10 ) % 3;
-switch(empCheck){
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HRS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HRS;
-        break;
-    default:
-        empHrs = 0;
-}
+empHrs = getWorkingHrs(empCheck);
 
 let empWage = empHrs * WAGE_PER_HR;
 console.log("Employee Wage: " + empWage);
